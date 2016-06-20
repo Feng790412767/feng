07dc038e8ae466b9a205405ff9e9683e168ecda4
zjf <790412767@qq.com>
2016-06-20 17:13:23 +0800

    创建类

---
+++ weibo/weibo/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weibo
+//
+//  Created by feng on 16/6/20.
+//  Copyright © 2016年 feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weibo/weibo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weibo
+//
+//  Created by feng on 16/6/20.
+//  Copyright © 2016年 feng. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
